190a8558f68c0ee1c0216f15577b65e430992031
Timur Magomedov <tim239@yandex.ru>
2018-01-05 00:26:31 +0300

    Load OpenCL source code

--- clrun.c
+++ clrun.c
@@ -2,15 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+void printUsage(const char* programName)
 {
-  printf("Hello world!\n");
-  if(argc != 2)
+  printf("Tester for OpenCL C runtime compilers behaviour. It compiles given program and calls all kernels inside it.\n");
+  printf("Usage: %s <OpenCL program file>\n", programName);
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
   {
-    printf("Usage: %s kernel_file\n", argv[0]);
+    printUsage(argv[0]);
     exit(EXIT_FAILURE);
   }
-  //TODO: load kernel to string
+  // Load OpenCL program source code to string
+  FILE* sourceFile = fopen(argv[1], "r");
+  if (!sourceFile)
+  {
+    perror(argv[1]);
+    printUsage(argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  fseek(sourceFile, 0, SEEK_END);
+  size_t size = ftell(sourceFile);
+  char *source = calloc(size + 1, sizeof(char));
+  if (!source)
+  {
+    fprintf(stderr, "Failed to allocate %zu bytes\n", size + 1);
+    exit(EXIT_FAILURE);
+  }
+  rewind(sourceFile);
+  fread(source, sizeof(char), size, sourceFile);
+  fclose(sourceFile);
+
+  printf("OpenCL source code:\n\"%s\"\n", source);
   //TODO: platforms loop
   //TODO: devices loop
   //TODO: build program
